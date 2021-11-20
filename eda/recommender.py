39ee99e014426c750c7674a56cd55801f147ae70
Kyle McLester <kmcleste@uncc.edu>
2021-11-20 18:25:45 -0500

    update filepath

--- eda/recommender.py
+++ eda/recommender.py
@@ -39,16 +39,16 @@ def main():
         + "- Recommend Movie by User: Returns list of recommendations for user_A based on user_B watch/rating history"
     )
     print(os.getcwd())
-    links = pd.read_csv("/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/links.csv", sep=",", encoding="latin-1")
-    movies = pd.read_csv("/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/movies.csv", sep=",", encoding="latin-1")
+    links = pd.read_csv("kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/links.csv", sep=",", encoding="latin-1")
+    movies = pd.read_csv("kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/movies.csv", sep=",", encoding="latin-1")
     ratings = pd.read_csv(
-        "/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/ratings.csv",
+        "kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/ratings.csv",
         sep=",",
         encoding="latin-1",
         usecols=["userId", "movieId", "rating"],
     )
     tags = pd.read_csv(
-        "/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/tags.csv",
+        "kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/tags.csv",
         sep=",",
         encoding="latin-1",
         usecols=["userId", "movieId", "tag"],
