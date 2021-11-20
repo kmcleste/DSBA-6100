819d0f815f963a454bb1eb3e5c1c65600b20dd6f
Kyle McLester <kmcleste@uncc.edu>
2021-11-20 18:47:17 -0500

    update filepath

--- eda/recommender.py
+++ eda/recommender.py
@@ -40,12 +40,13 @@ def main():
         + "- Recommend Movie by User: Returns list of recommendations for user_A based on user_B watch/rating history"
     )
     links = pd.read_csv("https://github.com/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/links.csv", sep=',', encoding='utf-8', error_bad_lines=False)
-    movies = pd.read_csv("https://github.com/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/movies.csv", sep=",", encoding="utf-8")
+    movies = pd.read_csv("https://github.com/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/movies.csv", sep=",", encoding="utf-8", error_bad_lines=False)
     ratings = pd.read_csv(
         "https://github.com/kmcleste/DSBA-6100/blob/main/eda/data/movielens/100k/ratings.csv",
         sep=",",
         encoding="utf-8",
         usecols=["userId", "movieId", "rating"],
+        error_bad_lines=False
     )
     # tags = pd.read_csv(
     #     cwd+"/data/movielens/100k/tags.csv",
